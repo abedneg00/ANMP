edd1c81a0acdeb677437881c93477f7242dba569
abedneg00 <abedtitusnego@gmail.com>
2024-02-22 14:28:11 +0700

    signup comment added

--- app/src/main/java/com/example/anmp_week1/SignUpActivity.kt
+++ app/src/main/java/com/example/anmp_week1/SignUpActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // SIGN UP FOR NEW USER
+        // 1. Cek existing account
+        // 2. Cek password
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
     }
